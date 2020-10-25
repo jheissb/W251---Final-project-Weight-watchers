c0adf3f69012c05a5bfe5933205ad65c7010ad77
Linda Yang <lindayang0416@Lindas-MacBook-Pro.local>
2020-10-24 22:34:57 -0700

    update file

--- imageCapture/dual_camera.py
+++ imageCapture/dual_camera.py
@@ -5,33 +5,28 @@
 # Using two  CSI cameras (such as the Raspberry Pi Version 2) connected to a
 # NVIDIA Jetson Nano Developer Kit (Rev B01) using OpenCV
 # Drivers for the camera and OpenCV are included in the base image in JetPack 4.3+
-
+​
 # This script will open a window and place the camera stream from each camera in a window
 # arranged horizontally.
 # The camera streams are each read in their own thread, as when done sequentially there
 # is a noticeable lag
 # For better performance, the next step would be to experiment with having the window display
 # in a separate thread
-
+​
 import cv2
 import threading
 import numpy as np
-import paho.mqtt.client as mqtt
-
+​
 # gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
 # Flip the image by setting the flip_method (most common values: 0 and 2)
 # display_width and display_height determine the size of each camera pane in the window on the screen
-
+​
 left_camera = None
 right_camera = None
-mqtt_client = None
-LOCAL_MQTT_HOST="ImageProcessorBroker"
-LOCAL_MQTT_PORT=1883
-LOCAL_MQTT_TOPIC="imageProcessor/captured"
-
-
-class STEREO_Camera:
-
+​
+​
+class CSI_Camera:
+​
     def __init__ (self) :
         # Initialize instance variables
         # OpenCV video capture element
@@ -43,7 +38,22 @@ class STEREO_Camera:
         self.read_thread = None
         self.read_lock = threading.Lock()
         self.running = False
-
+​
+​
+    def open(self, gstreamer_pipeline_string):
+        try:
+            self.video_capture = cv2.VideoCapture(
+                gstreamer_pipeline_string, cv2.CAP_GSTREAMER
+            )
+            
+        except RuntimeError:
+            self.video_capture = None
+            print("Unable to open camera")
+            print("Pipeline: " + gstreamer_pipeline_string)
+            return
+        # Grab the first frame to start the video capturing
+        self.grabbed, self.frame = self.video_capture.read()
+​
     def start(self):
         if self.running:
             print('Video capturing is already running')
@@ -54,23 +64,11 @@ class STEREO_Camera:
             self.read_thread = threading.Thread(target=self.updateCamera)
             self.read_thread.start()
         return self
-
-    def open(self, camera_index):
-        try:
-            self.video_capture = cv2.VideoCapture(camera_index)
-            
-        except RuntimeError:
-            self.video_capture = None
-            print("Unable to open camera")
-            return
-        # Grab the first frame to start the video capturing
-        self.grabbed, self.frame = self.video_capture.read()
-
-
+​
     def stop(self):
         self.running=False
         self.read_thread.join()
-
+​
     def updateCamera(self):
         # This is the thread to read images from the camera
         while self.running:
@@ -84,12 +82,13 @@ class STEREO_Camera:
         # FIX ME - stop and cleanup thread
         # Something bad happened
         
+​
     def read(self):
         with self.read_lock:
             frame = self.frame.copy()
             grabbed=self.grabbed
         return grabbed, frame
-
+​
     def release(self):
         if self.video_capture != None:
             self.video_capture.release()
@@ -97,76 +96,98 @@ class STEREO_Camera:
         # Now kill the thread
         if self.read_thread != None:
             self.read_thread.join()
-
-class MQTT_Sender:
-    client = None
-
-    def __init__ (self):
-        self.client = mqtt.Client()
-        self.client.on_connect = self.on_connect
-        self.client.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
-
-    def on_connect(self, client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
-        self.client.subscribe(LOCAL_MQTT_TOPIC)
-
-    def publish(self, payload):
-        self.client.publish(LOCAL_MQTT_TOPIC, payload, qos=1, retain=False)
-
-
-def init_mqtt_client():
-    mqtt_client = MQTT_Sender()
-
+​
+​
+# Currently there are setting frame rate on CSI Camera on Nano through gstreamer
+# Here we directly select sensor_mode 3 (1280x720, 59.9999 fps)
+def gstreamer_pipeline(
+    sensor_id=0,
+    sensor_mode=3,
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=30,
+    flip_method=2,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d sensor-mode=%d ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            sensor_mode,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+​
+​
 def start_cameras():
-    left_camera = STEREO_Camera()
-
-    left_camera.open(0)
-    
+    left_camera = CSI_Camera()
+    left_camera.open(
+        gstreamer_pipeline(
+            sensor_id=0,
+            sensor_mode=3,
+            flip_method=2,
+            display_height=540,
+            display_width=960,
+        )
+    )
     left_camera.start()
-
-    right_camera = STEREO_Camera()
-
-    right_camera.open(1)
-    
+​
+    right_camera = CSI_Camera()
+    right_camera.open(
+        gstreamer_pipeline(
+            sensor_id=1,
+            sensor_mode=3,
+            flip_method=2,
+            display_height=540,
+            display_width=960,
+        )
+    )
     right_camera.start()
-
-    cv2.namedWindow("STEREO Cameras", cv2.WINDOW_AUTOSIZE)
-
+​
+    cv2.namedWindow("CSI Cameras", cv2.WINDOW_AUTOSIZE)
+​
     if (
         not left_camera.video_capture.isOpened()
         or not right_camera.video_capture.isOpened()
     ):
         # Cameras did not open, or no camera attached
-
+​
         print("Unable to open any cameras")
         # TODO: Proper Cleanup
         SystemExit(0)
-
-    while cv2.getWindowProperty("STEREO Cameras", 0) >= 0 :
+​
+    while cv2.getWindowProperty("CSI Cameras", 0) >= 0 :
         
         _ , left_image=left_camera.read()
         _ , right_image=right_camera.read()
         camera_images = np.hstack((left_image, right_image))
-        # _ ,png = cv2.imencode('.png', camera_images)
-        # msg = png.tobytes()
-        # mqtt_client.publish(msg)
-        # print("Sent detected face to mosquitto")
-
-        cv2.imshow("STEREO Cameras", right_image)
-
+        cv2.imshow("CSI Cameras", camera_images)
+​
         # This also acts as
         keyCode = cv2.waitKey(30) & 0xFF
         # Stop the program on the ESC key
         if keyCode == 27:
             break
-
+​
     left_camera.stop()
     left_camera.release()
     right_camera.stop()
     right_camera.release()
     cv2.destroyAllWindows()
-
-
+​
+​
 if __name__ == "__main__":
-    # init_mqtt_client()
     start_cameras()
