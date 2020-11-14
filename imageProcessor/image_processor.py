a66f5b59a01de2381b34cfef43b74b8513d99c4b
Linda Yang <lindayang0416@Lindas-MacBook-Pro.local>
2020-11-13 22:48:40 -0800

    update processor

--- imageProcessor/image_processor.py
+++ imageProcessor/image_processor.py
@@ -1,9 +1,9 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
-LOCAL_MQTT_HOST="forwarderbroker"
+LOCAL_MQTT_HOST="172.18.0.2"
 LOCAL_MQTT_PORT=1883
-LOCAL_MQTT_TOPIC="facedetection/extractor"
+LOCAL_MQTT_TOPIC="imagedetection/extractor"
 
 REMOTE_MQTT_HOST="REPLACE_ME" # e.g: ec2-34-221-136-56.us-west-2.compute.amazonaws.com
 REMOTE_MQTT_TOPIC="facedetection/processor"
@@ -17,6 +17,7 @@ def on_message(client,userdata, msg):
     print("message received!")	
     # if we wanted to re-publish this message, something like this should work
     msg = msg.payload 
+    print(msg)
     # publish.single(REMOTE_MQTT_TOPIC, payload=msg, qos=1, retain=False, hostname=REMOTE_MQTT_HOST)
   except:
     print("Unexpected error:", sys.exc_info()[0])
