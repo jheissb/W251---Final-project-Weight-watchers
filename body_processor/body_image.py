7c8b1316a6fde64076cd2648d6c80e84160c149b
Linda Yang <lindayang0416@Lindas-MacBook-Pro.local>
2020-12-02 17:32:44 -0800

    update to include point on the image

--- body_processor/body_image.py
+++ body_processor/body_image.py
@@ -3,8 +3,9 @@ from datetime import datetime
 import json
 
 class BodyImage(object):
-    def __init__(self, raw_img, ratio):
+    def __init__(self, raw_img, processed_img, ratio):
         self.raw_img = raw_img
+        self.processed_img = processed_img
         self.image_id = str(uuid.uuid4())
         self.timestamp = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
         self.ratio = ratio
