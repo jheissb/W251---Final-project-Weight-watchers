465a4271799dade13fc1f7b0e3421db7a01ea7ed
Linda Yang <lindayang0416@Lindas-MacBook-Pro.local>
2020-12-04 22:56:57 -0800

    add basic logic

--- aggregator/aggregator.py
+++ aggregator/aggregator.py
@@ -4,3 +4,47 @@
 #get the face id
 #save and update the historical data from user historical database
 #return the historical data
+import paho.mqtt.client as mqtt
+import os
+import io
+
+REMOTE_MQTT_HOST="44.233.34.126"
+REMOTE_MQTT_PORT=1883
+REMOTE_MQTT_FACE_TOPIC="imagedetection/faceprocessor"
+REMOTE_MQTT_BODY_TOPIC="imagedetection/bodyprocessor"
+
+
+def on_connect_remote(client, userdata, flags, rc):
+        print("connected to remote broker with rc: " + str(rc))
+        client.subscribe(REMOTE_MQTT_FACE_TOPIC)
+        client.subscribe(REMOTE_MQTT_BODY_TOPIC)
+	
+def on_message(client,userdata,msg):
+  try:
+    print("message received!")	
+    try:
+      payload = io.BytesIO(msg.payload)
+      if msg.topic == REMOTE_MQTT_FACE_TOPIC:
+          return aggregate_face(payload)
+      if msg.topic == REMOTE_MQTT_BODY_TOPIC:
+          return aggregate_body(payload)
+      print('finish aggregator')
+    except Exception as e:
+      print(str(e))
+  except Exception as e:
+    print(str(e))
+
+def aggregate_body(msg):
+    return None
+
+def aggregate_face(msg):
+    return None
+
+remote_mqttclient = mqtt.Client()
+remote_mqttclient.on_connect = on_connect_remote
+remote_mqttclient.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT, 60)
+remote_mqttclient.on_message = on_message
+
+
+# go into a loop
+remote_mqttclient.loop_forever()
--- aggregator/s3util.py
+++ aggregator/s3util.py
@@ -1,1 +1,36 @@
 #THE CURD method for s3
+
+import paho.mqtt.client as mqtt
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime
+import uuid
+import os
+import io
+from PIL import Image as Image
+from array import array
+
+ACCESS_KEY_ID = os.getenv('ACCESS_KEY_ID')
+SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
+
+# s3_client = boto3.client('s3')
+s3_client = boto3.resource('s3',
+         aws_access_key_id=ACCESS_KEY_ID,
+         aws_secret_access_key= SECRET_ACCESS_KEY)
+
+S3_BUCKET_NAME='wait-watcher'
+
+def get_and_insurpt_body():
+    s3_client.Bucket(S3_BUCKET_NAME).put_object(Key=key, Body=img, ACL='public-read')
+
+def get_and_insurpt_face():
+    return None
+
+def retrive_files_by_folder():
+    return None
+
+def retrive_user_hitstorical_data_by_face_id():
+    return None
+
+def retrive_user_historical_data_by_date():
+    return None
