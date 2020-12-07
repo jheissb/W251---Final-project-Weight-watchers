7b76db69e53ae4475a64e8da444197ec3422e439
Linda Yang <yangf6@uw.edu>
2020-12-07 09:46:53 -0800

    update start script

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ docker-compose up -d
 local_broker_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' w251---final-project-weight-watchers_imageprocessorbroker_1)
 export local_broker_ip=$local_broker_ip
 echo $local_broker_ip
-python3 body_processor/body_processor.py
+cd body_processor
+python3 body_processor.py
 
